43d091dd6ec37cd69ce2a7553c5590001670b22a
jessicaa-huang <jeshuang@g.hmc.edu>
2024-06-10 22:39:21 -0700

    added top left logo

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import { InstagramLogo, InstagramMobileLogo } from '../../assets/constants'
+import { Link, Box, Flex, Image, Text } from '@chakra-ui/react'
 
 const Sidebar = () => {
   return (
@@ -11,8 +14,25 @@ const Sidebar = () => {
         left={0}
         px={{base:2, md:4}}
     >
-    <Flex>
-        
+    <Flex direction={"column"} gap={10} w={"full"}>
+        <Link to={"/"} as={RouterLink} pl={2} display={{base:"none", md:"block"}}
+        cursor={"pointer"} _hover={{ textDecoration: 'none' }}>
+            <Flex alignItems={"center"}>
+                <Image src="/sanrio-pfp.png" alt="my Logo" boxSize="40px"/>
+            {/* <InstagramLogo/> */}
+            <Text ml={2} fontWeight="bold">snackventure</Text>
+            </Flex>
+            
+        </Link>
+
+        <Link to={"/"} as={RouterLink} p={2} display={{base:"block", md:"none"}}
+        borderRadius={6} cursor={"pointer"} 
+        _hover={{bg:"whiteAlpha.200"}}
+        w={10}>
+            <Image src="/sanrio-pfp.png" alt="my Logo"/>
+
+            {/* <InstagramMobileLogo/> */}
+        </Link>
     </Flex>
     </Box>
   )
