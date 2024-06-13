b57d4d1bfff49b459ed47b257524eaf46fee83cb
jessicaa-huang <jeshuang@g.hmc.edu>
2024-06-12 23:54:54 -0700

    suggested user frontend finished

--- src/components/SuggestedUsers/SuggestedHeader.jsx
+++ src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { Avatar, Flex, Link, Text } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+import React from 'react';
+
 
 const SuggestedHeader = () => {
   return (
-    <div>SuggestedHeader</div>
+    <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
+      <Flex alignItems={"center"} gap={2}>
+        <Avatar name='jessica' size={'lg'} src='/sanrio-pfp.jpg'/>
+        <Text fontSize={12} fontWeight={"bold"}>
+          jessica_huang
+        </Text>
+      </Flex>
+      <Link
+        as={RouterLink}
+        to={"/auth"}
+        fontSize={14}
+        fontWeight={"medium"}
+        color={"blue.400"}
+        style={{textDecoration: "none"}}
+        cursor={"pointer"}
+      >
+          Log out
+      </Link>
+    </Flex>
   )
 }
 
